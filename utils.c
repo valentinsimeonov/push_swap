e43b86bed47ee02b828be787352ef3e501d56d1c
Valentin Simeonov <vsimeono@c2s4d11.42wolfsburg.de>
2022-02-01 11:56:28 +0100

    Added Stack Operations + Unfinished Arg Checker

---
+++ utils.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vsimeono <vsimeono@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/23 16:01:25 by vsimeono          #+#    #+#             */
+/*   Updated: 2022/01/31 21:41:24 by vsimeono         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	ft_atoi(const char *str)
+{
+	int		i;
+	int		num;
+	int		sign;
+
+	i = 0;
+	sign = 1;
+	num = 0;
+	while ((*(str + i) >= 9 && *(str + i) <= 13) || (*(str + i) == ' '))
+	{
+		i++;
+	}
+	if (*(str + i) == '-' || *(str + i) == '+')
+	{
+		if (*(str + i) == '-')
+		{
+			sign = sign * (-1);
+		}
+		i++;
+	}
+	while ((*(str + i) >= 48 && *(str + i) <= 57))
+	{
+		num = (num * 10) + (*(str + i) - '0');
+		i++;
+	}
+	return (num * sign);
+}
+
+
+// t_stack	*ft_lstnew(void *content)
+// {
+// 	t_list	*new_element;
+
+// 	new_element = (t_list *)malloc(sizeof(t_list) * 1);
+// 	if (new_element)
+// 	{
+// 		new_element->content = content;
+// 		new_element->next = NULL;
+// 	}
+// 	return (new_element);
+// }
+
+void	ft_lstadd_back(t_stack **lst, t_stack *new)
+{
+	t_stack	*temp;
+
+	temp = *lst;
+	if (*lst)
+	{
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new;
+	}
+	else
+		*lst = new;
+}
+
+int	ft_isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	return (0);
+}
